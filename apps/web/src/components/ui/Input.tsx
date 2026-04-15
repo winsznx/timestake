cdc757b2f1c4fa3bf444106a25f6f4434d3f4276
winsznx <timjosh507@gmail.com>
2026-04-15 21:21:15 +0100

    refactor(web): import cn from dedicated module in Input

--- apps/web/src/components/ui/Input.tsx
+++ apps/web/src/components/ui/Input.tsx
@@ -1,6 +1,6 @@
 import type { InputHTMLAttributes } from 'react';
 
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/cn';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
