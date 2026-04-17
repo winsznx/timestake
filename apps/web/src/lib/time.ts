d56e5087fce04ff3c8f333ac4eb3fc4c9108f236
winsznx <timjosh507@gmail.com>
2026-04-17 01:03:53 +0100

    feat(web): add time duration constants

---
+++ apps/web/src/lib/time.ts
@@ -0,0 +1,7 @@
+export const SECOND_MS = 1000;
+export const MINUTE_MS = 60 * SECOND_MS;
+export const HOUR_MS = 60 * MINUTE_MS;
+export const DAY_MS = 24 * HOUR_MS;
+export const WEEK_MS = 7 * DAY_MS;
+
+export const STACKS_BLOCK_TIME_MS = 10 * MINUTE_MS;
