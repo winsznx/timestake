922ad6a3f08fc1ad6c4fd79d226bfac2c32768b5
winsznx <timjosh507@gmail.com>
2026-04-18 01:54:19 +0100

    chore(web): add components/habits barrel export

---
+++ apps/web/src/components/habits/index.ts
@@ -0,0 +1,6 @@
+export * from './CheckInButton.tsx';
+export * from './HabitCalendar.tsx';
+export * from './HabitCard.tsx';
+export * from './HabitForm.tsx';
+export * from './HabitList.tsx';
+export * from './StreakCounter.tsx';
